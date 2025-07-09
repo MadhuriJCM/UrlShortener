b07cb457b08c1746f91b80379e3eeb83a1d50c6b
Madhuri J M <106056118+MadhuriJCM@users.noreply.github.com>
2025-07-09 12:21:20 +0530

    Delete src/main/java/com/example/urlshortener directory

--- src/main/java/com/example/urlshortener/ShortUrlRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.urlshortener;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShortUrlRepository extends JpaRepository<ShortUrl, String> {
-}
